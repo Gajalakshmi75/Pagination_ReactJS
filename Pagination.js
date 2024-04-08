24ef9defc0a847ea4a0434e972a703fe3cf95595
Gajalakshmi Yarramsetti <126240213+Gajalakshmi75@users.noreply.github.com>
2024-04-08 13:39:07 +0530

    Add files via upload

---
+++ Pagination.js
@@ -0,0 +1,93 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import './paginationn.css';
+
+// function Pagination() {
+//     const [posts,setPosts]=useState([]);
+//     const[loading,setLoading]=useState(false);
+//     const [currentPage,setCurrentPage]=useState(1);
+//     const [postperpage,setPostperpage]=useState(10);
+
+//     useEffect(()=>{
+//         const fetchposts= async () => {
+//             setLoading(true);
+//             const  res = await  axios.get('https://jsonplaceholder.typicode.com/posts');
+//             setPosts(res.data);
+//             setLoading(false);
+//         }
+//         fetchposts();
+//     },[ ] );
+//     console.log(posts);
+//   return (
+//     <div>Pagination</div>
+//   )
+// }
+
+// export default Pagination;
+
+import React, { useEffect, useState } from 'react';
+import './paginationn.css';
+
+function Pagination() {
+    const [products,setProducts]=useState([]);
+    const [page,setPage]=useState(1);
+
+    const fetchproducts = async ()=>{
+        const res= await fetch ("https://dummyjson.com/products");
+        const data= await res.json();
+        // console.log(data);
+        if (data && data.products){
+            setProducts(data.products);
+        };
+
+    };
+    console.log(products);
+
+     useEffect(()=>{
+        fetchproducts();
+     },[]);
+
+     const selectpageHandler = (selectedPage) => {
+        if(selectedPage >= 1 && selectedPage<=products.length / 10  && selectedPage !==page)
+        setPage(selectedPage);
+
+     };
+
+
+  return (
+    <div>
+        {
+            products.length > 0 && 
+            <div className='products'>
+                {
+                    products.slice(page * 10 -10 ,page * 10).map((prod)=>{
+                        return <span className='products__single' key={prod.id}>
+                            <img src={prod.thumbnail} alt={prod.title}/>
+                            <span>{prod.title}</span>
+                        </span>
+                    })}
+                    </div>
+        }
+        {
+            products.length > 0 && <div className='pagination'>
+                <span 
+                className={page > 1 ? " " :"pagination__disable"}
+                onClick={()=>selectpageHandler(page-1)}>◀</span>
+                {
+                    [...Array(products.length / 10 )].map((_, i)=>{
+                        return <span 
+                        className={page === i + 1 ? "pagination__selected" : " "}
+                        onClick={()=>selectpageHandler(i + 1)} key={i}>{i + 1}</span>;
+                    })
+                }
+                <span onClick={()=>selectpageHandler(page+1)}
+                className={page < products.length / 10 ? " " :"pagination__disable"}
+                >▶</span>
+
+            </div>
+        }
+    </div>
+  );
+}
+
+export default Pagination;
